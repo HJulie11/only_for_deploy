e9403e42e82832b9ec0582138725cbc724a1f4d9
HJulie11 <Hseojin0711@gmail.com>
2024-09-05 22:18:08 +0900

    start script added

--- backend/server.js
+++ backend/server.js
@@ -67,8 +67,14 @@ app.get('/', (req, res) => {
     res.send('API Working')
 })
 
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Server started on http://localhost:${port}`)
+// })
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on ${PORT}`);
+});
 
 // mongodb+srv://gyeongcheong23:OMIM5vasaz7gnC84@cluster0.d8iwxjc.mongodb.net/?
