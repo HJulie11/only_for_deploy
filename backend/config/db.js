4deeb6bfb62249db22e7e811e51d85f6f85aa999
HJulie11 <Hseojin0711@gmail.com>
2024-09-05 12:59:41 +0900

    mongoose connect vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
  
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://gyeongcheong23:OMIM5vasaz7gnC84@cluster0.d8iwxjc.mongodb.net/gyeongcheong").then(() => console.log('DB connected'));
+    // await mongoose.connect("mongodb+srv://gyeongcheong23:OMIM5vasaz7gnC84@cluster0.d8iwxjc.mongodb.net/gyeongcheong").then(() => console.log('DB connected'));
+    console.log(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI).then(() => console.log('DB connected'));
 }
