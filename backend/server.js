bd9e1d4f5e186076a041f69cca30407d93f0d592
HJulie11 <Hseojin0711@gmail.com>
2024-09-06 23:40:10 +0900

    trying backend debug (vercel)

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,11 @@ const port = 4000
  
 //MIDDLEWARE
 app.use(express.json())
-app.use(cors({
-    origin: 'https://www.gyeong-cheong.com',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'https://www.gyeongcheong.com',
+//     credentials: true
+// }))
+app.use(cors())
 app.use(express.static('uploads'));
 
 // Middleware to parse JSON bodies
